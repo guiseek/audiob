8808bd9821c5e8b62cc4a1bcc68ca10ea06b8973
Gui Seek <guiseek@gmail.com>
2022-02-26 10:56:53 -0300

    initial commit

---
+++ apps/audiob/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'audiob-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'audiob';
+}
